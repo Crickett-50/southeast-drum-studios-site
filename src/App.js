ed4e33670dddae9f088fa9d1fe02dc94dff4cbed
Crickett-50 <alexcrickett@yahoo.com>
2025-07-29 13:12:09 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Southeast Drum Studios</h1>
+      <p>Coming Soon...</p>
+    </div>
+  );
+}
+
+export default App;
